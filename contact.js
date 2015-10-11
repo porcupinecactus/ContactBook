10d195357d96acaf6a6be34555390015c1ffb3e7
Nathan Vann <nathanvann89@gmail.com>
2015-10-11 14:09:52 -0700

    functionality of me and cats address is there and the ability to add contacts is there

---
+++ contact.js
@@ -0,0 +1,26 @@
+angular.module('contactApp',[])
+	.controller('contactCtrl', function($scope){
+		var contacts = this;
+
+		contacts.entries = [
+		{fName: 'Nathan', lName: 'Vann', email: 'nathanvann89@gmail.com', phone: '9187743621'},
+		{fName: 'Cat', lName: 'Thomas', email: 'misscatthomas@gmail.com', phone: '1234567894'}
+		];
+
+		contacts.heading = ['Name', 'Email', 'Phone Number'];
+
+		contacts.addEntry= function(){
+			console.log('This fired');
+			contacts.entries.push(
+				{
+				fName:contacts.entries.fName,
+				lName:contacts.entries.lName,
+				email:contacts.entries.email,
+				phone:contacts.entries.phone
+				});
+			contacts.entries.fName = "";
+			contacts.entries.lName = "";
+			contacts.entries.email = "";
+			contacts.entries.phone = "";
+		};
+	});
